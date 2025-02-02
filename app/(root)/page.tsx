c95043d9c090c49a63efdcc92ded3c58e18134f8
Daffa Hanifisyafiq <daffa@mkpmobile.com>
2025-02-03 00:21:36 +0700

    feat: navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,13 @@
-import React from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constant/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
+
   console.log(session);
 
   return (
     <>
-      <div className="px-10 pt-20">
-        <form
-          className="px-10 pt-[100px]"
-          action={async () => {
-            "use server";
-
-            await signOut({ redirectTo: ROUTES.SIGN_IN });
-          }}
-        >
-          <Button type="submit">Log out</Button>
-        </form>
-      </div>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
     </>
   );
 };
